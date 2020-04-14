20977988aa0f023232668a58e07ab02a6b8bbf8a
Hongru Zhai <hongru.zhai.1378@student.uu.se>
2020-04-15 01:20:42 +0200

    start of simulation study

--- change point analysis.r
+++ change point analysis.r
@@ -1,9 +1,13 @@
-###simulating LNP dataset
+######Simulating LNP dataset and implementing change point analysis###
+####https://cran.r-project.org/web/packages/ecp/###
+library(ggplot2)
+library(tidyverse)
+library(mvtnorm)
+library(Rcpp)
+library(ecp)
+###2 field of views, 72 time points, 8 channels(data replicated 8x)
 
-
-
-
-
+set.seed(46)
 
 
 
